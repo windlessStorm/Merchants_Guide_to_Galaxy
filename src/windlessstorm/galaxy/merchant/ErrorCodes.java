62d2e13115cc670f8a3967f8be0cb16795d31953
windlessStorm <vikrant.biswas@gmail.com>
2015-12-15 04:21:46 +0530

    Create ErrorCodes.java

---
+++ src/windlessstorm/galaxy/merchant/ErrorCodes.java
@@ -0,0 +1,19 @@
+package windlessstorm.galaxy.merchant;
+
+public enum ErrorCodes {
+	
+	SUCCESS,
+	
+	NO_INPUT,
+	
+	INVALID,
+	
+	INVALID_ROMAN_CHARACTER,
+	
+	INVALID_ROMAN_STRING,
+	
+	INVALID_LINE,
+	
+	NO_IDEA
+
+}
